b6198aab2d74e4696ac1f3887eabd279506ce6e5
Jingsong.fu <Jinsong.fu@shanbay.com>
2018-06-04 19:10:26 +0800

    udpate

--- day2/nimbleTest.js
+++ day2/nimbleTest.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 function downlaod(version, destination, callback) {
     var url = 'http://nodejs.org/dist/node-v' + version + '.tar.gz';
     var filePath = destination + '/' + version + '.tgz';
-    exec('curl ' + url + ' >' + filePath, callback);
+    exec('curl ' + url + ' >' + filePath,callback);
 }
 
 flow.series([
@@ -12,21 +12,27 @@ flow.series([
         flow.parallel([
             function (callback) {
                 console.log('Downloading V0.4.6....');
-                downlaod('0.4.6', '/tmp', callback);
+                downlaod('0.4.6', '/tmp',function(res){
+                    console.log('Download V0.4.6....');
+                    callback();
+                });
             },
             function (callback) {
                 console.log('Downloading V0.4.7....');
-                downlaod('0.4.7', '/tmp', callback);
+                downlaod('0.4.7', '/tmp',function(res){
+                    console.log('Download V0.4.7....');
+                    callback();
+                });
             }
-        ], callback);
+        ],callback);
     },
-    function (callback) {
+    function () {
         console.log('archive downloaded files...');
         exec(
             'tar cvf node_distros.tar /tmp/0.4.6.tgz /tmp/0.4.7.tgz',
             function (error, stdout, stderr) {
                 console.log('all done!');
-                callback('all done callback1');
+                // callback();
             }
         );
     }
