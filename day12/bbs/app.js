e31dfa6d968a9b8b9eb5eb53f2e913c2f5c841ee
KinsomyJS <FJS0420@outlook.com>
2018-07-18 14:35:21 +0800

    update day13

--- day12/bbs/app.js
+++ day12/bbs/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var register = require('./routes/register');
+var messages = require('./lib/messages');
 
 var app = express();
 
@@ -15,9 +18,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(messages());
+app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- day13/README.md
+++ day13/README.md
@@ -17,3 +17,27 @@ app.post('/register',register.submit);
 ### 创建注册表单html
 在views文件夹下创建register.ejs。
 
+### 回话存放临时消息
+```js
+var express = require('express');
+var res = express.response;
+
+res.message = function (msg, type) {
+    type = type || 'info';
+    var sess = this.req.session;
+    sess.message = sess.message || [];
+    sess.message.push({ type: type, string: msg });
+};
+
+res.error = function (msg) {
+    return this.message(msg, 'error');
+};
+
+module.exports = function(req,res,next){
+    res.locals.messages = req.session.messages || [];
+    res.locals.removeMessages = function(){
+        req.session.messages = [];
+    };
+    next();
+};
+```
